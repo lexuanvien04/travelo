b904d3484f0f45a0e4b5d67baba18c80eb737402
lexuanvien <lexuanvien04@gmail.com>
2023-01-09 22:43:44 +0700

    Signin and signup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Home from './Component/Home/Home';
+import Signup from './Component/Signin/Signup';
 
 
 
 export default function App(){
   return(
     <>
+      {/* <Home /> */}
       <Home />
     </>
   );
